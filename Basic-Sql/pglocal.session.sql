00bb10da7720d46233f37e2720cf6247e5a3fe2c
MananPoojara <billybutcher215@gmail.com>
2025-06-09 23:20:17 +0530

    week 3 assignment

--- Basic-Sql/pglocal.session.sql
+++ Basic-Sql/pglocal.session.sql
@@ -7,6 +7,7 @@ CREATE TABLE sales(
     revenue DECIMAL(10, 2)   
 )
 
+
 -- INSERT VALUES INTO sales
 INSERT INTO sales (id, sales_date, product, quantity, revenue) VALUES
 (1, '2023-01-01', 'Widget A', 10, 100.00),
@@ -15,8 +16,10 @@ INSERT INTO sales (id, sales_date, product, quantity, revenue) VALUES
 (4, '2023-01-04', 'Widget A', 15, 150.00),
 (5, '2023-01-05', 'Widget B', 8, 80.00);
 
+
 --SELECT QUERIES
 SELECT * FROM sales;
+
 
 -- Retrieve data only for a specific product, such as "Product A":
 SELECT * FROM sales WHERE product = 'Widget A';
@@ -25,6 +28,22 @@ SELECT * FROM sales WHERE product = 'Widget A';
 --Retrieve the total revenue for each day:
 SELECT sales_date, SUM(revenue) FROM sales GROUP BY sales_date ORDER BY sales_date;
 
+
 --Retrieve the total quantity sold for each product:
 SELECT product, SUM(quantity) FROM sales GROUP BY product ORDER BY SUM(quantity);
+
+
+--Operators : Use various oprators 
+SELECT * FROM sales WHERE product = 'Widget A';
+--AND for matching conditions something 
+SELECT * FROM sales WHERE product = 'Widget A' AND revenue < 300;
+-- SELECT * FROM sales WHERE product = 'Widget A' AND revenue <= 100;
+--OR : for selecting any one condition 
+SELECT * FROM sales WHERE product = 'Widget A' OR revenue < 300;
+--Between And : for selecting range 
+SELECT * FROM sales WHERE sales_date Between '2023-01-02' AND '2023-01-04';
+SELECT * FROM sales WHERE quantity BETWEEN 5 AND 10;
+--Like : For selecting recorde with pattern contidion 
+--below query is fetching data where the product name start with Wid..
+SELECT * FROM sales WHERE product LIKE 'Wid%';
 
